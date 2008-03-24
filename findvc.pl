56fefbb2457a85b1c2d0f2769fe97585ef10bb0c
Christian Stratowa <cstrato@aon.at>
2008-03-24 20:31:10 +0000

    update to version xps-0.99.4 - updated files: config.win, findvc.pl
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/xps@30642 bc3139a8-67e5-0310-9ffc-ced21a209358

--- findvc.pl
+++ findvc.pl
@@ -1,17 +1,19 @@
 use strict;
 use File::Find;
 
-# get path containing "..\VC\BIN"
-my @path = split(';', $ENV{PATH});
+# get path containing "..\VC\bin"
+my @path = split(';', $ENV{MSVSPATH});
 my $pathvc = "";
 my $i;
 for $i (0..$#path) {
-   if ($path[$i] =~ /VC\\BIN$/) {
+#print ("path: $path[$i]\n"); ####test
+   if ($path[$i] =~ /VC\\bin$/) {
       $pathvc = $path[$i];
       last;
    }#if
 }#for
-#print ("found path: $pathvc\n");
+#print ("found pathvc: $pathvc\n"); ####test
+#print ("found path: @path\n"); ####test
 
 # input file names
 my $file2find   = "cl.exe";
@@ -19,27 +21,42 @@ my @directories = ($pathvc);
 
 # output file names
 my $file_found = "";
+my $name_found = "";
 my $dir_found  = "";
 
 # check if VC++ is installed in fixed location
 find(\&wanted, @directories);
 
+#print ("file_found: $file_found\n"); ####test
+#print ("name_found: $name_found\n"); ####test
+#print ("dir_found: $dir_found\n"); ####test
+
+# return result
+if (!($file_found =~ /^cl.exe$/)) {
+   print("ERR_CLEXE");
+   exit;
+}#if
+
+# check for file vcvarsall.bat
+@path      = split('\\\bin', $dir_found);
+$file2find = "vcvarsall.bat";
+find(\&wanted, @path);
+
+# return result
+if ($file_found =~ /^vcvarsall.bat$/) {
+#   print ("$name_found");
+   print ("$dir_found");
+} else {
+   print("ERR_VCVARSALLEXE");
+}#if
+
+
+#--- subroutine wanted ---#
 sub wanted {
    if ($_ =~ /$file2find$/) {
       $file_found = $_;
+      $name_found = $File::Find::name;
       $dir_found  = $File::Find::dir;
       last;
    }#if
 }#sub
-
-# return result
-if ($file_found =~ /^cl.exe$/) {
-   print ("$file_found");
-} else {
-   print("ERR_CLEXE");
-}#if
-
-
-
-
-
