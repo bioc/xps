c677ae7b9fd49f075e7f6037a9a10244224a56b7
Lori Shepherd <lori.shepherd@roswellpark.org>
2020-09-09 08:22:56 -0400

    User requested deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -7,11 +7,17 @@
 #   require(utils);
 #}
 
+#.onAttach <- function(libname, pkgname) {
+#   packageStartupMessage(paste("\nWelcome to", pkgname, "version", packageDescription(pkgname, fields="Version")));
+#   packageStartupMessage("    an R wrapper for XPS - eXpression Profiling System");
+#   packageStartupMessage("    (c) Copyright 2001-2018 by Christian Stratowa");
+#   packageStartupMessage("    ");
+#}
 .onAttach <- function(libname, pkgname) {
-   packageStartupMessage(paste("\nWelcome to", pkgname, "version", packageDescription(pkgname, fields="Version")));
-   packageStartupMessage("    an R wrapper for XPS - eXpression Profiling System");
-   packageStartupMessage("    (c) Copyright 2001-2018 by Christian Stratowa");
-   packageStartupMessage("    ");
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.13")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
 }
 
 .onUnload <- function(libpath) {
